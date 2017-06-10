b0e4b17d760647139b55704578365e835c235b80
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 11:21:03 +0200

    commit

--- js/zmiennetablicowe.js
+++ js/zmiennetablicowe.js
@@ -1,1 +1,20 @@
 'use strict'
+
+var imiona = new Array();
+var imionaStudentow = new Array(9);
+var imionaChlopakow = new Array('Mateusz', 'Michał', 'Krzysiek', 'Sergio');
+
+imionaStudentow[2]= "Przemek";
+
+console.log(imiona);
+console.log(imionaStudentow);
+console.log(imionaChlopakow);
+
+var wiek = []; /* to samo co new Array()  */
+var wiekStudentow = [20, 25, 28];
+
+console.log(wiek);
+console.log(wiekStudentow);
+
+
+
